7bb1753b5a78d5f97221ac2dfdbbf22aa66dc520
Arpit Anand <marpit1510@gmail.com>
2026-05-26 05:51:31 +0530

    Add App class with main method and add function

---
+++ src/main/java/com/iarpit/App.java
@@ -0,0 +1,11 @@
+package com.iarpit;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from ArPiT-1310 CI/CD Pipeline!");
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
